fd8c3f40c7afcffb3eb54d72ae8dc8d483887dc5
Tarun Arora <tananrules@gmail.com>
2015-08-05 12:32:03 +0530

    Cardslib i1

--- app/src/main/java/com/example/tanan/articlescards/ArticlesFragment.java
+++ app/src/main/java/com/example/tanan/articlescards/ArticlesFragment.java
@@ -1,68 +1,43 @@
 package com.example.tanan.articlescards;
 
 import android.app.Fragment;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
-import retrofit.RestAdapter;
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.recyclerview.internal.CardArrayRecyclerViewAdapter;
+import it.gmariotti.cardslib.library.recyclerview.view.CardRecyclerView;
 
 /**
  * Created by tanan on 4/8/15.
  */
 public class ArticlesFragment extends Fragment {
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private LinearLayoutManager linearLayoutManager;
-    private PostsObject mPostsObject;
+    CardArrayRecyclerViewAdapter cardArrayRecyclerViewAdapter;
+    PostsObject mPostsObject;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.cards_fragment, container, false);
 
-        recyclerView = (RecyclerView) view.findViewById(R.id.cards_recycler_view);
-        recyclerView.setHasFixedSize(true);
-
-        linearLayoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        mPostsObject = new PostsObject();
-        mPostsObject.posts = new ArrayList<>();
-        adapter = new ArticleAdapter(getActivity(), mPostsObject);
-        recyclerView.setAdapter(adapter);
-
-        new GetArticles().execute();
-
+        CardRecyclerView cardRecyclerView = (CardRecyclerView) view.findViewById(R.id.card_recycler_view);
+        cardArrayRecyclerViewAdapter = new CardArrayRecyclerViewAdapter(getActivity(), getCards());
+        cardRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        if(cardRecyclerView != null) {
+            cardRecyclerView.setAdapter(cardArrayRecyclerViewAdapter);
+        }
         return view;
 
     }
 
-    public class GetArticles extends AsyncTask<Void, Void, PostsObject> {
-
-        @Override
-        protected PostsObject doInBackground(Void... params) {
-
-            RestAdapter restAdapter = new RestAdapter.Builder()
-                    .setEndpoint("http://blog.teamtreehouse.com")
-                    .build();
-
-            ArticlesService articlesService = restAdapter.create(ArticlesService.class);
-
-            return articlesService.getArticles();
-        }
-
-        @Override
-        protected void onPostExecute(PostsObject postsObject) {
-            super.onPostExecute(postsObject);
-            mPostsObject.posts.addAll(postsObject.posts);
-            adapter.notifyDataSetChanged();
-        }
+    public ArrayList<Card> getCards() {
+        ArrayList<Card> cards = new ArrayList<>();
+        return cards;
     }
 }
